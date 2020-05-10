f913309d55febb13640388f340d5817b2e229435
aayurt <aayurtshrestha@gmail.com>
2020-05-10 18:32:05 +0545

    changes fro content add

--- src/app/Layout/Menu.js
+++ src/app/Layout/Menu.js
@@ -1,15 +1,19 @@
 import React from "react";
 import "./Menu.css";
+import Content from "../router/component/content/content";
 
 export default function Menu({ pass }) {
-  console.log("pass", pass.passShow());
   return (
     <>
       <div
         className={pass.passShow() ? "left-menu-bar" : "left-menu-bar close"}
         onMouseLeave={() => pass.passSetShow(false)}
       >
-        {pass.passShow() ? "MENU" : null}
+        {pass.passShow() ? (
+          <>
+            <Content />
+          </>
+        ) : null}
       </div>
     </>
   );
--- src/app/router/component/content/content.js
+++ src/app/router/component/content/content.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Logout from "../container/logoutContainer";
 export default function Content() {
   return (
     <>
       <center>SMS</center>
+      <br />
+      <center>
+        <Logout />
+      </center>
     </>
   );
 }
